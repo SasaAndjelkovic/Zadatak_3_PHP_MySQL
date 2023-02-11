f9f65c8fda74a262756f1e10be309c1570c795ea
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-02-11 19:43:03 +0100

    Priprema nadogradnje postojećeg sistema (iz zadatka modula 2,3) za rad sa bazom

---
+++ view/home.php
@@ -0,0 +1,112 @@
+<?php
+
+trait Welcome {
+
+
+    static function datum() {
+        $datum = date ("l jS \of F Y h:i:s A");
+        return $datum;
+        }
+        
+    static function quantity() {
+        $quantity = rand(1, 100);
+        return $quantity;
+        }
+        
+    static function poruka() {
+        $msg = "Izvestaj o prodaji se sastoji od dva podataka: datum izvestaja i kolicina prodaje"; 
+        return strtoupper($msg);
+    }
+}
+
+echo "<h1>Izvestaj o prodaji</h1>";
+echo Welcome::poruka();
+echo "<hr>";
+echo Welcome::datum();
+echo "<hr>";
+echo Welcome::quantity();
+echo "<hr>";
+
+//Welcome::_toString($msg, $quantity, $datum);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+
+    <div>
+        <h1>Teatar na brdu - Predstave</h1>
+    </div>
+
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Naziv predstave</th>
+                    <th>Opis</th>
+                    <th>Autor</th>
+                    <th>Avatar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($_SESSION['predstave'] as $predstave) :
+                ?>
+                    <tr>
+                        <td> <?php echo $predstave->getId(); ?></td>
+                        <td> <?php echo $predstave->getName(); ?></td>
+                        <td> <?php echo $predstave->getDescription(); ?> </td>
+                        <td> <?php echo $predstave->getAuthor(); ?></td>
+                        <td> <?php print_r($predstave->getSpisakAvatara()[0]->getName());  ?>
+                    </tr>
+                <?php
+
+                endforeach;
+
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+    <div>
+        <h1>CUD operacije</h1>
+
+        <form action="" method="get">
+            <input type="text" name="izmeni" size="25" placeholder="Upisi ID predstave za izmenu">
+            <button>Izmeni</button>
+            <br>
+        </form>
+
+        <form action="" method="get">
+            <input type="text" name="izbrisi" size="25" placeholder="Upisi ID predstave za brisanje">
+            <button>Obrisi</button>
+            <br>
+        </form>
+
+    </div>
+
+    <div>
+        <a href="?addShow">
+            <button>Dodaj novu predstavu</button>
+        </a>
+    </div>
+    <br>
+
+
+    <br>
+    <a href="?logout">
+        <button>Logout</button>
+    </a>
+
+</body>
+
+</html>

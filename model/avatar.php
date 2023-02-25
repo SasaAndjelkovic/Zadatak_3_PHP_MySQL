6820db2800c91a7b056623f3795f1e3767e8c600
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-02-25 09:54:12 +0100

    definisanje klase Avatar sa funkcijom getAvatar

--- model/avatar.php
+++ model/avatar.php
@@ -1,12 +1,15 @@
 <?php
 
-class Avatar extends BasicInformation implements TheaterInformation {
+class Avatar {
 
     private $description;
+	private $avatarID;
+	private $avatarName;
     
-    public function __construct($id, $name, $description) {
-        parent::__construct($id, $name);
+    public function __construct($avatarID, $avatarName, $description) {
         $this->description = $description;
+        $this->avatarID = $avatarID;
+        $this->avatarName = $avatarName;
     }
 	
 	public function getDescription() {
@@ -18,18 +21,25 @@ class Avatar extends BasicInformation implements TheaterInformation {
 	}
 	
 	public function getId() {
-        return $this->id;
+        return $this->avatarID;
 	}
 	
 	public function setId($id_variable) {
-        $this->id=$id_variable;
+        $this->avatarID=$id_variable;
 	}
 	
 	public function getName() {
-        return $this->name;
+        return $this->avatarName;
 	}
 	
 	public function setName($name_variable) {
-        $this->name = $name_variable;
+        $this->avatarName = $name_variable;
 	}
+
+	public static function getAvatar(mysqli $conn)
+    {
+        $q = "SELECT * FROM avatar";
+        return $conn->query($q);
+    }
+
 }
